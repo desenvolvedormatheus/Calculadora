1b961a2b47364cbd558a7d3ad2bc8d21038caf53
Matheus Pereira de Souza <matheus.souza.smm@gmail.com>
2022-08-08 15:58:08 -0300

    0.9

--- Index/Script/script.js
+++ Index/Script/script.js
@@ -1,9 +1,37 @@
+const menu = document.querySelector('.menu')
+const menutema = document.querySelector('.menutema')
+const corpo = document.querySelector('.corpo')
+const sessao1 = document.querySelector('.sessao1')
+const btnmenu = document.querySelector('.btnmenu')
+const sessao2 = document.querySelector('.sessao2')
+
 function abrirmenu() {
-    var menu = document.querySelector('.menu')
-    menu.style.right = '1rem'
+    menu.style.right = '0.5rem'
+}
+function fecharmenu() {
+    menu.style.right = '-6rem'
 }
 
-function fecharmenu() {
-    var menu = document.querySelector('.menu')
-    menu.style.right = '-9rem'
+function btntema() {
+    menutema.style.right = '0.5rem'
+}
+
+function temaescuro() {
+    menutema.style.right = '-5rem'
+    corpo.style.background = '#232323'
+    sessao1.style.background = '#575757'
+    btnmenu.style.background = '#232323'
+    menu.style.background = '#232323'
+    menutema.style.background = '#232323'
+    sessao2.style.color = '#fff'
+}
+
+function temaclaro() {
+    menutema.style.right = '-5rem'
+    corpo.style.background = '#ffffff'
+    sessao1.style.background = '#c8c8c8ff'
+    btnmenu.style.background = '#ffffff'
+    menu.style.background = '#ffffff'
+    menutema.style.background = '#ffffff'
+    sessao2.style.color = '#000'
 }
